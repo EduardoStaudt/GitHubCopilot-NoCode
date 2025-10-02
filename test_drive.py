009b48ecbed27453f528f1d473fa80d3cda2d997
Eduardo Staudt <programadorstaudt@gmail.com>
2025-10-02 16:03:05 -0300

    Commit inicial

---
+++ test_drive.py
@@ -0,0 +1,6 @@
+# Create a function that sorts a list of numbers in ascending order
+def sort_numbers(numbers):
+    return sorted(numbers)
+
+# Simple function to multiply two numbers
+def multiply(a, b):
